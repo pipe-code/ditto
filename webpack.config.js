84470a070bd0e74d4088bec725435c0313335402
pipe-code <felipea.0420@gmail.com>
2023-08-17 08:21:00 -0500

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: './js/main.js',
+    entry: ['./js/main.js'],
+    mode: 'development',
     output: {
-        path: __dirname + '/js',
-        filename: 'main.bundle.js'
+        path: path.resolve(__dirname, 'js'),
+        filename: 'main.bundle.js',
+        publicPath: 'auto'
     },
-
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                loader: [
-                    {loader: MiniCssExtractPlugin.loader},
-                    {loader: 'css-loader'},
-                    {loader: 'sass-loader'}
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-                loader: 'url-loader?limit=100000'
-            }
-        ]
-    },
-
     plugins: [
         new MiniCssExtractPlugin({
             filename: '../css/main.bundle.css'
-        })
-    ]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)$/i,
+                type: "asset"
+            }
+        ]
+    }
 }
