463cc26e3de43201d7c2a228bda1dfebe35fba9f
pipe-code <felipea.0420@gmail.com>
2024-10-29 15:34:36 -0500

    [feat] update packages and unused meta tags removed

--- header.php
+++ header.php
@@ -16,8 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-  <meta name="description" content="">
-  <meta name="author" content="">
   <meta name="viewport" content="width=device-width">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
